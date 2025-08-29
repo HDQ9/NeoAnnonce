b488f3fff428a128466e44d6557e5957d39a91ba
HDQ <93005407+HDQ9@users.noreply.github.com>
2025-08-30 01:53:57 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,16 +1,10 @@
--- Nom du fichier : client.lua
--- Description : Reçoit l'ordre du serveur et le transmet à l'interface (NUI).
-
--- Enregistre un événement réseau que le serveur peut appeler
 RegisterNetEvent('cyberAnnonce:afficher')
 
--- Cette fonction est appelée lorsque le serveur déclenche l'événement 'cyberAnnonce:afficher'
 AddEventHandler('cyberAnnonce:afficher', function(message)
-    -- Envoie un message à l'interface NUI (le fichier HTML)
-    -- avec l'action 'afficher' et le contenu du message.
     SendNUIMessage({
         action = "afficher",
         texte = message
     })
 end)
+
 
