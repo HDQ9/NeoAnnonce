6dca8bd3c450bfdee00fdd5dcf43c81baccd54ac
HDQ <93005407+HDQ9@users.noreply.github.com>
2025-08-30 01:54:53 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,16 @@
--- Nom du fichier : fxmanifest.lua
--- Description : Fichier de manifeste qui définit la ressource pour FiveM.
-
 fx_version 'cerulean'
 game 'gta5'
 
-author 'VotreNom'
+author 'NeoSupportV1'
 description 'Système d\'annonce cyberpunk avec synthèse vocale'
 version '1.0.0'
 
--- Fichier principal de l'interface utilisateur
 ui_page 'html/index.html'
 
--- Fichiers que l'interface utilisateur doit charger
 files {
     'html/index.html'
 }
 
--- Scripts côté client et serveur
 client_script 'client.lua'
 server_script 'server.lua'
+
